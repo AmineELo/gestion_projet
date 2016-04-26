4f94841ed20f890834637ffd385010ea80607da0
Amine Elouattar <elouattar@gmail.com>
2016-04-27 00:27:41 +0100

    template set up

---
+++ pages/includes/side_bar.php
@@ -0,0 +1,19 @@
+
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li>
+                            <a href="index.php"><i class="fa fa-home fa-fw"></i> Accueil</a>
+                        </li>
+                        <li>
+                            <a href="utilisateurs.php"><i class="fa fa-users fa-fw"></i> Utilisateurs</a>
+                        </li>
+                        <li>
+                            <a href="mes_documents.php"><i class="fa fa-paperclip fa-fw"></i> Mes documents</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
---
+++ pages/utilisateurs.php
@@ -0,0 +1,164 @@
+<?php
+  include 'includes/header.php';
+  include 'includes/side_bar.php';
+?>
+<div id="page-wrapper">
+  <div class="row">
+      <div class="col-md-12">
+          <h1 class="page-header">Utilisateurs</h1>
+      </div>
+      <!-- /.col-lg-12 -->
+  </div>
+  <div class="row">
+      <div class="col-lg-12">
+          <div class="panel panel-default">
+              <div class="panel-body">
+                  <div class="dataTable_wrapper">
+                      <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                          <thead>
+                              <tr>
+                                  <th>Nom</th>
+                                  <th>Prénom</th>
+                                  <th>Propriétaire de</th>
+                                  <th>Membre de</th>
+                              </tr>
+                          </thead>
+                          <tbody>
+                              <tr class="odd gradeX">
+                                  <td>Zniber</td>
+                                  <td>Oumaima</td>
+                                  <td>Projet 1</td>
+                                  <td>Projet 7</td>
+                              </tr>
+                              <tr class="even gradeC">
+                                  <td>Zniber</td>
+                                  <td>Oumaima</td>
+                                  <td>Projet 1</td>
+                                  <td>Projet 7</td>
+                              </tr>
+                              <tr class="odd gradeA">
+                                  <td>Zniber</td>
+                                  <td>Oumaima</td>
+                                  <td>Projet 1</td>
+                                  <td>Projet 7</td>
+                              </tr>
+                              <tr class="even gradeA">
+                                  <td>Zniber</td>
+                                  <td>Oumaima</td>
+                                  <td>Projet 1</td>
+                                  <td>Projet 7</td>
+                              </tr>
+                              <tr class="odd gradeA">
+                                  <td>Zniber</td>
+                                  <td>Oumaima</td>
+                                  <td>Projet 1</td>
+                                  <td>Projet 7</td>
+                              </tr>
+                              <tr class="even gradeA">
+                                  <td>Zniber</td>
+                                  <td>Oumaima</td>
+                                  <td>Projet 1</td>
+                                  <td>Projet 7</td>
+                              </tr>
+                              <tr class="gradeA">
+                                  <td>Zniber</td>
+                                  <td>Oumaima</td>
+                                  <td>Projet 1</td>
+                                  <td>Projet 7</td>
+                              </tr>
+                              <tr class="gradeA">
+                                  <td>Zniber</td>
+                                  <td>Oumaima</td>
+                                  <td>Projet 1</td>
+                                  <td>Projet 7</td>
+                              </tr>
+                              <tr class="gradeA">
+                                  <td>Zniber</td>
+                                  <td>Oumaima</td>
+                                  <td>Projet 1</td>
+                                  <td>Projet 7</td>
+                              </tr>
+                              <tr class="gradeA">
+                                  <td>Zniber</td>
+                                  <td>Oumaima</td>
+                                  <td>Projet 1</td>
+                                  <td>Projet 7</td>
+                              </tr>
+                              <tr class="gradeA">
+                                  <td>Zniber</td>
+                                  <td>Oumaima</td>
+                                  <td>Projet 1</td>
+                                  <td>Projet 7</td>
+                              </tr>
+
+                              <tr class="odd gradeA">
+                                  <td>Zniber</td>
+                                  <td>Oumaima</td>
+                                  <td>Projet 1</td>
+                                  <td>Projet 7</td>
+                              </tr>
+                              <tr class="even gradeA">
+                                  <td>Zniber</td>
+                                  <td>Oumaima</td>
+                                  <td>Projet 1</td>
+                                  <td>Projet 7</td>
+                              </tr>
+                              <tr class="odd gradeA">
+                                  <td>Zniber</td>
+                                  <td>Oumaima</td>
+                                  <td>Projet 1</td>
+                                  <td>Projet 7</td>
+                              </tr>
+                              <tr class="even gradeA">
+                                  <td>Zniber</td>
+                                  <td>Oumaima</td>
+                                  <td>Projet 1</td>
+                                  <td>Projet 7</td>
+                              </tr>
+                              <tr class="gradeA">
+                                  <td>Zniber</td>
+                                  <td>Oumaima</td>
+                                  <td>Projet 1</td>
+                                  <td>Projet 7</td>
+                              </tr>
+                              <tr class="gradeA">
+                                  <td>Zniber</td>
+                                  <td>Oumaima</td>
+                                  <td>Projet 1</td>
+                                  <td>Projet 7</td>
+                              </tr>
+                              <tr class="gradeA">
+                                  <td>Zniber</td>
+                                  <td>Oumaima</td>
+                                  <td>Projet 1</td>
+                                  <td>Projet 7</td>
+                              </tr>
+                              <tr class="gradeA">
+                                  <td>Zniber</td>
+                                  <td>Oumaima</td>
+                                  <td>Projet 1</td>
+                                  <td>Projet 7</td>
+                              </tr>
+                              <tr class="gradeA">
+                                  <td>Zniber</td>
+                                  <td>Oumaima</td>
+                                  <td>Projet 1</td>
+                                  <td>Projet 7</td>
+                              </tr>
+
+                          </tbody>
+                      </table>
+                  </div>
+                  <!-- /.table-responsive -->
+              </div>
+              <!-- /.panel-body -->
+          </div>
+          <!-- /.panel -->
+      </div>
+      <!-- /.col-lg-12 -->
+  </div>
+  <!-- /.row -->
+</div>
+<?php
+  include 'includes/footer.php';
+?>
